1d55f14337f16fb46b9b36ac44430608aa1ef2cf
Cheyenne Pillay <Cheyenneivan@gmail.com>
2016-03-23 01:08:11 +0200

    setting up array pointer  and assigning values.

--- Tut5-Employee/mainTest.cpp
+++ Tut5-Employee/mainTest.cpp
@@ -10,4 +10,15 @@ int main(){
 	SalaryEmployee *eSal = new SalaryEmployee("Cheyenne", 214513360);
 	CommissionEmployee *eCo = new CommissionEmployee("Raina", 214563254);
 
+	// pointer for array
+	Employee *arrayMkr[3];
+	// set salary
+	eSal->setSalary(20000);
+	//set hourly attributes
+	eHr->setHourlyRate(15000);
+	eHr->setHoursWorked(20);
+	// set commission atributes
+	eCo->setBaseSalary(5000);
+	eCo->setRate(0.40);
+	eCo->setRevenue(12000.6);
 }
