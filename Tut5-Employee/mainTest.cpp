1ca4ab968dc73137845fba5fac077b0f19162154
Cheyenne Pillay <Cheyenneivan@gmail.com>
2016-03-23 01:06:17 +0200

    assigning values for employees

--- Tut5-Employee/mainTest.cpp
+++ Tut5-Employee/mainTest.cpp
@@ -2,3 +2,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
+int main(){
+
+
+	// assigning values for employees
+	HourlyEmployee *eHr = new HourlyEmployee("Thykara", 214584562);
+	SalaryEmployee *eSal = new SalaryEmployee("Cheyenne", 214513360);
+	CommissionEmployee *eCo = new CommissionEmployee("Raina", 214563254);
+
+}
