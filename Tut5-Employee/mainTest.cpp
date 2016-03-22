2ef34ce6f1f353975cf0d2e405231894ed67f41c
Cheyenne Pillay <Cheyenneivan@gmail.com>
2016-03-23 01:09:56 +0200

    filling the array  with objects  and displays the employee and salary details

--- Tut5-Employee/mainTest.cpp
+++ Tut5-Employee/mainTest.cpp
@@ -10,8 +10,18 @@ int main(){
 	SalaryEmployee *eSal = new SalaryEmployee("Cheyenne", 214513360);
 	CommissionEmployee *eCo = new CommissionEmployee("Raina", 214563254);
 
+
+
 	// pointer for array
 	Employee *arrayMkr[3];
+
+
+
+	//fill array with  3 objects
+	arrayMkr[0] = eSal;
+	arrayMkr[1] = eHr;
+	arrayMkr[2] = eCo;
+
 	// set salary
 	eSal->setSalary(20000);
 	//set hourly attributes
@@ -21,4 +31,21 @@ int main(){
 	eCo->setBaseSalary(5000);
 	eCo->setRate(0.40);
 	eCo->setRevenue(12000.6);
+
+
+
+	//display employee and salary details
+	cout << endl;
+	cout << "Salary employee: " << eSal->name() << " " << eSal->staffNumber() << " -- " << eSal->name() << ", your income is " << arrayMkr[0]->salary() << " a month" << endl;
+	cout << endl;
+
+	cout << "Hourly employee: " << eHr->name() << " " << eHr->staffNumber() << " -- " << eHr->name() << ", your income is " << arrayMkr[1]->salary() << " a month" << endl;
+	cout << endl;
+	cout << "Commission employee: " << eCo->name() << " " << eCo->staffNumber() << " -- " << eCo->name() << ", your income is " << arrayMkr[2]->salary() << " a month" << endl;
+	cout << endl;
+
+	cout << "_____________________________________________________________________________________________________________________" << endl;
+	cout << "Total number of employees: " << Employee::numberOfEmployees << endl;
+	cout << endl;
+
 }
